79c85144aebcf8c5ee72aa321092c932652061ff
symbolon-dev <44700110+symbolon-dev@users.noreply.github.com>
2025-12-10 16:18:01 +0100

    Add basic sveltekit app

--- apps/web-sveltekit/eslint.config.js
+++ apps/web-sveltekit/eslint.config.js
@@ -1,4 +1,7 @@
 import antfu from '@antfu/eslint-config'
 import { baseOptions } from '@repo/config/eslint'
 
-export default antfu(baseOptions, { svelte: true })
+export default antfu({
+    ...baseOptions,
+    svelte: true,
+})
